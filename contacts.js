dc3f42cc4dfea371089197320e4bd8522e7ca624
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-09-16 20:29:33 +0300

    add function remove

--- contacts.js
+++ contacts.js
@@ -12,19 +12,26 @@ const { nanoid } = require("nanoid");
     const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
     return contacts;
-    }
+};
    
  
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const currentContact = contacts.find((contact) => contact.id === contactId);
+  const contactIndex = contacts.find((contact) => contact.id === contactId);
 
-  return currentContact || null;
-  
-}
-function removeContact(contactId) {
-  
-}
+  return contactIndex || null;
+};
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(contact => contact.id === contactId);
+  if (contactIndex === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(contactIndex, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
 async function addContact({ name, email, phone }) {
   const contacts = await listContacts();
